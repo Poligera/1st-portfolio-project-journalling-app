9fa257502e29b5e1fcdcc337ef2f78c428c1300c
moony98 <84647389+moony98@users.noreply.github.com>
2021-07-02 17:16:03 +0100

    getting the server to work

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const { model } = require("./model");
+const data = require("../data.json")
 
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => res.send(data))
 
 module.exports = app;
