2e64de40fcb149942f9af6e3471650c2f3844f52
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-07-05 09:27:29 +0100

    init

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { model } = require("./model");
+const Post = require("./model");
 const data = require("../data.json")
 
 //MIDDLEWARE
@@ -9,5 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send(data))
+
+Post.addPost({message: "hi"})
+console.log(Post.all)
 
 module.exports = app;
