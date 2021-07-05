dbc2ddbf9740ad520a4ae664b1d42d6c7c7c3588
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-07-05 12:03:14 +0100

    add routes

--- server/model.js
+++ server/model.js
@@ -5,7 +5,7 @@ class Post {
     this.id = data.id;
     this.message = data.message;
     this.reactions = {happy: 0, sad: 0, shocked: 0};
-    this.comments = [];
+    this.comments = data.comments; //! This is for testing, change to empty array later
   }
   static get all() {
     return postData
@@ -15,6 +15,24 @@ class Post {
     const newPost = new Post({id: id, ...data});
     postData.push(newPost);
   }
+  static getPost(id) {
+    const posts = Post.all;
+    for (let i = 0; i < posts.length; i++) {
+      if (posts[i].id === id) {
+        return posts[i]
+      }
+    }
+  }
+  static addComment(id, comment) {
+    const targetPost = Post.getPost(id);
+    targetPost.comments.push(comment)
+  }
+  static updateReactions(id, targetReaction) {
+    const targetPost = Post.getPost(id);
+    targetPost.reaction[targetReaction] ++
+    return targetPost
+  }
+
 }
 
 module.exports = Post;
